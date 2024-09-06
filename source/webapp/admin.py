99a108edb3307b8dc6a0248f5105b7aff8ac5d69
Korenchuk Rustam <smohghtxx@gmail.com>
2024-09-06 15:30:39 +0600

    регестрация админки

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from webapp.models.category import Category
+from webapp.models.part import Part
+from webapp.models.user import User
+from webapp.models.car import CarModel, CarBrand
+from webapp.models.engine import Engine
+from webapp.models.country import Country
+from webapp.models.vehicleinfo import VehicleInfo
 
-# Register your model here.
+
+admin.site.register(Category)
+admin.site.register(Part)
+admin.site.register(User)
+admin.site.register(CarModel)
+admin.site.register(CarBrand)
+admin.site.register(Engine)
+admin.site.register(Country)
+admin.site.register(VehicleInfo)
