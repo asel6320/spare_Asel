8d47c3acaf37bfe10b4154e010fd4dae2ac5d023
Bekbol <19kani92@gmail.com>
2024-09-23 20:08:31 +0600

    удалил лишние миграции, сделал корректную миграцию, учитывая прошлые миграции
    чуть изменил логику в представлениях, чтобы соединить с юзером авторизованным и не авторизованным
    
    чуть подкорректировал модели

--- source/webapp/models/cart.py
+++ source/webapp/models/cart.py
@@ -1,9 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
 from django.db import models
-from django.urls import reverse
+
+User = get_user_model()
 
 
 class Cart(models.Model):
+    session_key = models.CharField(max_length=40, null=True, blank=True, verbose_name='Ключ сессии')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Пользователь")
     quantity = models.PositiveIntegerField(verbose_name='Количество', default=1, validators=(MinValueValidator(1),))
     part = models.ForeignKey('webapp.Part', related_name='carts', on_delete=models.CASCADE, verbose_name="запчасти")
 
--- source/webapp/views/cart.py
+++ source/webapp/views/cart.py
@@ -3,8 +3,6 @@ from django.shortcuts import redirect, get_object_or_404, render
 from django.views import View
 from django.db.models import Sum, F, Subquery, OuterRef
 
-from webapp.models.order import Order
-
 
 class CartAdd(View):  # добавление запчасти в корзину
 
@@ -12,31 +10,44 @@ class CartAdd(View):  # добавление запчасти в корзину
         self.part = get_object_or_404(Part, pk=kwargs.get('pk'))
         return super().dispatch(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):  # добавление товара в корзину с проверкой наличия на складе
+    def post(self, request, *args, **kwargs):
         if self.part.amount > 0:
-            # Здесь может быть фильтрация корзины по пользователю, если корзина привязана к юзеру
-            order, created = Order.objects.get_or_create(user=request.user)
-            cart, created = Cart.objects.get_or_create(part=self.part, order=order)
+            if request.user.is_authenticated:
+                cart, created = Cart.objects.get_or_create(part=self.part, user=request.user)
+            else:
+                session_key = request.session.session_key
+                if not session_key:
+                    request.session.create()
+                cart, created = Cart.objects.get_or_create(part=self.part, session_key=request.session.session_key)
+
             if not created:
                 if cart.quantity < self.part.amount:
                     cart.quantity += 1
             else:
                 cart.quantity = 1
             cart.save()
+
         return redirect('webapp:parts_list')
 
 
 class CartView(View):
 
     def get(self, request, *args, **kwargs):
-        carts = Cart.objects.all()
-        latest_price_subquery = PriceHistory.objects.filter(part=OuterRef('part')).order_by('-date_changed').values(
-            'price')[:1]
+        if request.user.is_authenticated:
+            carts = Cart.objects.filter(user=request.user)
+        else:
+            session_key = request.session.session_key
+            if not session_key:
+                request.session.create()
+            carts = Cart.objects.filter(session_key=request.session.session_key)
+
+        latest_price_subquery = PriceHistory.objects.filter(part=OuterRef('part')).order_by('-date_changed').values('price')[:1]
         carts_with_price = carts.annotate(latest_price=Subquery(latest_price_subquery))
 
         total = carts_with_price.aggregate(
             total_sum=Sum(F('quantity') * F('latest_price'))
         )['total_sum']
+
         return render(request, 'cart/cart_view.html', {'carts': carts_with_price, 'total': total})
 
 
