32abd8fedfbd9a5adb829eaf9b0da24d9f41457c
Bekbol <19kani92@gmail.com>
2024-09-24 18:37:35 +0600

    создал создание юзера в определенном количестве

---
+++ source/accounts/factory/user_factory.py
@@ -0,0 +1,18 @@
+import factory
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+
+User = get_user_model()
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    username = factory.Faker('username')
+    password = factory.Sequence(lambda n: f'password{n}')
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        kwargs['password'] = make_password(kwargs['password'])
+        return super(UserFactory, cls)._create(model_class, *args, **kwargs)
+
+    class Meta:
+        model = User
