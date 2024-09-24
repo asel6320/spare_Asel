5bc5d18cd49434df7ba7bc32645ce3eb6d5550d7
Bekbol <19kani92@gmail.com>
2024-09-24 15:11:59 +0600

    добавил валидацию в форму

--- source/webapp/forms/order_form.py
+++ source/webapp/forms/order_form.py
@@ -1,9 +1,21 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from webapp.models import Order
 
 
 class OrderForm(forms.ModelForm):
+
+    def clean(self):
+        cleaned_data = super().clean()
+        required_fields = ["email", "phone", "last_name", "first_name"]
+
+        for field in required_fields:
+            if not cleaned_data.get(field):
+                raise ValidationError(f"Поле {field} не может быть пустым.")
+
+        return cleaned_data
+
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'phone', 'email']
