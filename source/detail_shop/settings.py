ad71158879e31d403751f85196bc3e7f03e05d6b
ABM1204 <bekabek13@bk.ru>
2024-10-04 00:57:12 +0600

    Добавил базовые фильтры. Использовал библиотку django-unfold для кастомизации админки

--- source/detail_shop/settings.py
+++ source/detail_shop/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "unfold",
+    "unfold.contrib.filters",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,18 +1,54 @@
 from django.contrib import admin
+from django.shortcuts import render
+from django.urls import path
+
 from webapp.models.category import Category
 from webapp.models.part import Part
 from webapp.models.car import CarModel, CarBrand
 from webapp.models.engine import Engine
 from webapp.models.country import Country
 from webapp.models.vehicleinfo import VehicleInfo
-from webapp.models import Review
+from webapp.models import Review, Order
 
 
 admin.site.register(Category)
-admin.site.register(Part)
-admin.site.register(CarModel)
 admin.site.register(CarBrand)
 admin.site.register(Engine)
 admin.site.register(Country)
-admin.site.register(VehicleInfo)
-admin.site.register(Review)
+
+class PartAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'vehicle_info', 'amount')
+    list_filter = ('category', 'vehicle_info__model__brand', 'amount')
+
+admin.site.register(Part, PartAdmin)
+
+
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand', 'year_of_manufacture')
+    list_filter = ('brand', 'year_of_manufacture')
+
+admin.site.register(CarModel, CarModelAdmin)
+
+
+class VehicleInfoAdmin(admin.ModelAdmin):
+    list_display = ('vehicle_type', 'model', 'year_of_manufacture', 'engine')
+    list_filter = ('vehicle_type', 'year_of_manufacture', 'engine')
+
+admin.site.register(VehicleInfo, VehicleInfoAdmin)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('part', 'user', 'text')
+    list_filter = ('part', 'user')
+
+admin.site.register(Review, ReviewAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'last_name', 'created_at')
+    list_filter = ('user', 'created_at')
+
+admin.site.register(Order, OrderAdmin)
+
+
+
