1d8fd3c74fc3b378f8ee88c29a7472c9ef4aadda
Bekbol <19kani92@gmail.com>
2024-10-10 16:44:12 +0600

    привел в порядок регистрацию(модель, форму и представление)

--- source/accounts/forms/registration.py
+++ source/accounts/forms/registration.py
@@ -15,7 +15,6 @@ class RegisterForm(UserCreationForm):
     username = forms.CharField()
     email = forms.CharField()
     phone_number = forms.CharField()
-
     password1 = forms.CharField()
     password2 = forms.CharField()
 
@@ -24,3 +23,15 @@ class RegisterForm(UserCreationForm):
         if User.objects.filter(phone_number=phone_number).exists():
             raise forms.ValidationError("Пользователь с этим номером телефона уже зарегистрирован.")
         return phone_number
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("Пользователь с таким никнеймом уже зарегистрирован.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Пользователь с таким email уже зарегистрирован.")
+        return email
--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from accounts.views.logout import custom_logout_view
 from accounts.views.login import CustomLoginView
-from accounts.views.register import register_view
+from accounts.views.register import UserRegistrationView
 
 from accounts.views.profile_edit import profile_edit_view
 from accounts.views.profile_view import profile_view
@@ -12,7 +12,7 @@ from accounts.views.password_change_view import CustomPasswordChangeView
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', register_view, name='register'),
+    path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', custom_logout_view, name='logout'),
     path('profile/', profile_view, name='profile_view'),
