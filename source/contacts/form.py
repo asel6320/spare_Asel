a7acb6c14f41059933f3c85f9f559765f6e46cf9
Kairat <ischenbaew@gmai.com>
2024-10-18 12:08:18 +0200

    создал форму

---
+++ source/contacts/form.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import CallbackRequest
+
+class CallbackRequestForm(forms.ModelForm):
+    class Meta:
+        model = CallbackRequest
+        fields = ['name', 'phone', 'comments']
