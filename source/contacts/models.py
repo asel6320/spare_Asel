7bcd7288d3a2e4c88e2862333ad4c6f8e6698a88
Kairat <ischenbaew@gmai.com>
2024-10-18 12:08:07 +0200

    создал модель

--- source/contacts/models.py
+++ source/contacts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ContactRequest(models.Model):
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия")
+    phone_number = models.CharField(max_length=20, verbose_name="Номер телефона")
+    comments = models.TextField(blank=True, null=True, verbose_name="Комментарии")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата запроса")
+
+    def __str__(self):
+        return f"{self.last_name} ({self.phone_number})"
