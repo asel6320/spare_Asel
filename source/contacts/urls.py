9cea15060b4304835315252b30080a082b193fd7
Kairat <ischenbaew@gmai.com>
2024-10-20 19:01:22 +0200

    Создал маршруты urls.py

---
+++ source/contacts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from contacts.views import contact_request
+
+app_name = 'contacts'
+
+
+urlpatterns = [
+    path('contact/', contact_request, name='contact'),
+]
+
