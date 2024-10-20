50659554bc987a8d417923ab7b16e48349c2bc14
Kairat <ischenbaew@gmai.com>
2024-10-20 19:02:22 +0200

    поключил маршруты contacts в detail_shop

--- source/detail_shop/urls.py
+++ source/detail_shop/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('order/', include('orders.urls')),
     path('', include('part.urls')),
     path('accounts/', include("accounts.urls")),
+    path('contacts/', include('contacts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
