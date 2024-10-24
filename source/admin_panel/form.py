d1e32063efca761b82b565657abfedd4494db752
Bekbol <19kani92@gmail.com>
2024-10-24 19:51:26 +0600

    реализовал логику с процентами, увеличить на и смену цены

--- source/admin_panel/form.py
+++ source/admin_panel/form.py
@@ -2,4 +2,35 @@ from django import forms
 
 
 class PriceUpdateForm(forms.Form):
-    price = forms.DecimalField(max_digits=1000, decimal_places=2, label='Новая цена', required=True)
+    price = forms.DecimalField(
+        required=False,
+        decimal_places=2,
+        max_digits=10,
+        widget=forms.NumberInput(attrs={
+            'placeholder': 'Новая цена',
+            'step': '0.01'
+        }),
+        label='Введите процент изменения'
+    )
+    percentage = forms.DecimalField(
+        required=False,
+        decimal_places=2,
+        max_digits=10,
+        widget=forms.NumberInput(attrs={
+            'placeholder': 'Процент изменения',
+            'step': '0.01'
+        }),
+        label='Введите процент изменения'
+    )
+
+    # Поле для изменения цены до определенного значения
+    price_to = forms.DecimalField(
+        required=False,
+        decimal_places=2,
+        max_digits=10,
+        widget=forms.NumberInput(attrs={
+            'placeholder': 'Сумма изменения',
+            'step': '0.01'
+        }),
+        label='Введите сумму изменения'
+    )
