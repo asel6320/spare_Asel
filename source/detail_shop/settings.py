6f4d849652d4b2d7d162fd77553af40d4e28a3c3
ABM1204 <bekabek13@bk.ru>
2024-10-24 20:32:56 +0600

    создал образ и контейнеры для доекра

--- source/detail_shop/settings.py
+++ source/detail_shop/settings.py
@@ -89,11 +89,11 @@ WSGI_APPLICATION = 'detail_shop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env.str("NAME"),
-        'USER': env.str("USER"),
-        'PASSWORD': env.str("PASSWORD"),
-        'HOST': env.str("HOST"),
-        'PORT': env.int("PORT"),
+        'NAME': env.str("POSTGRES_DB"),
+        'USER': env.str("POSTGRES_USER"),
+        'PASSWORD': env.str("POSTGRES_PASSWORD"),
+        'HOST': env.str("DB_HOST"),
+        'PORT': env.int("DB_PORT"),
     }
 }
 
