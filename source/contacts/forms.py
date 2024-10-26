c2a4913e46718f4286470d94e5f86d8d335c3d99
Kairat <ischenbaew@gmai.com>
2024-10-26 12:06:09 +0200

    Обновлена форма

--- source/contacts/forms.py
+++ source/contacts/forms.py
@@ -4,14 +4,18 @@ from .models import ContactRequest
 class ContactRequestForm(forms.ModelForm):
     class Meta:
         model = ContactRequest
-        fields = ['last_name', 'phone_number', 'comments']
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'comments']
         widgets = {
+            'first_name': forms.TextInput(attrs={'placeholder': 'Введите ваше имя'}),
             'last_name': forms.TextInput(attrs={'placeholder': 'Введите вашу фамилию'}),
+            'email':forms.EmailInput(attrs={'placeholder': 'Email'}),
             'phone_number': forms.TextInput(attrs={'placeholder': 'Введите ваш номер телефона'}),
             'comments': forms.TextInput(attrs={'placeholder': 'Оставьте комментарии'}),
         }
         labels = {
+            'first_name': 'Имя',
             'last_name': 'Фамилия',
+            'email': 'Email',
             'phone_number': 'Номер телефона',
             'comments': 'Комментарии'
         }
