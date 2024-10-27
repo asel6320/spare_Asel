98517b753fc9cbc80108aa2a16a7dcdb729fb567
Kairat <ischenbaew@gmai.com>
2024-10-27 18:10:08 +0100

    Добавил маршруты newsletter в основное приложение

--- source/newsletter/views.py
+++ source/newsletter/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Subscription
+from django.contrib import messages
 
-# Create your views here.
+def subscribe(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+
+        if Subscription.objects.filter(email=email).exists():
+            messages.error(request, "Этот email уже подписан.")
+        else:
+            Subscription.objects.create(email=email)
+            messages.success(request, "Спасибо за подписку!")
+
+        return redirect('part:parts_list')
+    return render(request, 'base.html')
+
