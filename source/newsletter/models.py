8f493fde7867e6af0c769a86764ea466f96f0e73
Kairat <ischenbaew@gmai.com>
2024-10-27 18:09:09 +0100

    Написал модели для приложения newsletter

--- source/newsletter/models.py
+++ source/newsletter/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Subscription(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    subscription_date = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.email
