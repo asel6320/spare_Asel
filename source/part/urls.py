af2c0256d2be76164d5ea5ea6e2cc5f81c14744f
ABM1204 <bekabek13@bk.ru>
2024-10-29 18:23:20 +0600

    Временно убрал кэшировние

--- source/part/urls.py
+++ source/part/urls.py
@@ -6,7 +6,7 @@ from part.views import PartsListView, PartsDetailView, PartsMainView
 app_name = 'part'
 
 urlpatterns = [
-    path('', cache_page(60)(PartsListView.as_view()), name='parts_list'),
+    path('', PartsListView.as_view(), name='parts_list'),
     path('part/<int:pk>/', cache_page(60*3)(PartsDetailView.as_view()), name='part_detail'),
-    path('parts/', cache_page(60*5)(PartsMainView.as_view()), name='parts_main'),
+    path('parts/', PartsMainView.as_view(), name='parts_main'),
 ]
