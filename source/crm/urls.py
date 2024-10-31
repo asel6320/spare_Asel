ab988ee79aba90305a754ac1a47510fd38d1ae38
Asel Amatova <asel6320@gmail.com>
2024-10-31 07:53:43 -0700

    SCRUM-84: Я завершила работу над функциональностью аналитики в CRM, а также добавила кнопку для создания нового заказа.
    Аналитика:
    
    Реализована панель аналитики с отображением общей статистики заказов:
    Всего заказов
    Завершённых заказов
    Ожидающих заказов
    Добавлены блоки с аналитикой по доставке и оплате:
    Заказы с доставкой
    Оплаченные заказы
    Реализована информация о доходах:
    Общее количество проданных товаров
    Общий доход
    Средняя стоимость заказа
    Графики:
    График заказов за последние 30 дней.
    График новых пользователей за последние 30 дней.
    Топ-5 популярных товаров.
    Кнопка "Добавить новый заказ":
    
    Кнопка была добавлена в заголовок страницы "История заказов".
    При нажатии на кнопку осуществляется переход на страницу для добавления нового заказа.
    Проблемы:
    В процессе работы была исправлена ошибка AttributeError, связанная с некорректной инициализацией данных формы.
    Обновлен метод form_valid в классе для обработки создания нового заказа, чтобы корректно сохранять данные и связывать заказ с пользователем (завтра доделаю)

--- source/crm/urls.py
+++ source/crm/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.views.decorators.cache import cache_page
 
-from crm.views import CustomerListView, OrderListView
+from crm.views import CustomerListView, OrderListView, AnalyticsView, AddOrderView
 
 app_name = 'crm'
 
 urlpatterns = [
     path('customers/', CustomerListView.as_view(), name='customers'),
     path('orders/', OrderListView.as_view(), name='orders'),
+    path('analytics/', AnalyticsView.as_view(), name='analytics'),
+    path('orders/add/', AddOrderView.as_view(), name='add_order')
 ]
