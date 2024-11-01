e3d8154c92c34ce3a1b83acff09b28524bb2737a
Kairat <ischenbaew@gmai.com>
2024-11-01 23:27:21 +0100

    настроил параметры SMTP-сервера для отправки писем.

--- source/detail_shop/settings.py
+++ source/detail_shop/settings.py
@@ -173,3 +173,13 @@ AUTH_USER_MODEL = 'accounts.User'
 
 LOGOUT_REDIRECT_URL = '/'
 
+
+# это для рассылки
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'partsauto312@gmail.com'
+EMAIL_HOST_PASSWORD = 'upfanzkikgofjghn'
+
+
