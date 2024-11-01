65fcafb5b224e151b450d0af186ee597d9ef2fa6
Kairat <ischenbaew@gmai.com>
2024-11-01 23:28:47 +0100

    создан обработчик subscribe для обработки запросов на подписку, сохранения данных в базу и отправки email-подтверждения

--- source/newsletter/views.py
+++ source/newsletter/views.py
@@ -1,18 +1,38 @@
 from django.shortcuts import render, redirect
-from .models import Subscription
+from django.core.mail import send_mail
 from django.contrib import messages
+from .forms import SubscriptionForm
+from .models import Subscription
+
 
 def subscribe(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            name = form.cleaned_data['name']
 
-        if Subscription.objects.filter(email=email).exists():
-            messages.error(request, "Этот email уже подписан.")
-        else:
-            Subscription.objects.create(email=email)
-            messages.success(request, "Спасибо за подписку!")
+            if Subscription.objects.filter(email=email).exists():
+                messages.warning(request, "Вы уже подписаны на рассылку!")
+            else:
+                form.save()
 
-        return redirect('part:parts_list')
-    return render(request, 'base.html')
+                send_confirmation_email(name,email)
 
+                messages.success(request, "Вы успешно подписались на рассылку!")
+            return redirect('part:parts_list')
+    else:
+        form = SubscriptionForm()
+
+    return render(request, 'subcribe.html', {'form': form})
+
+
+def send_confirmation_email(name, email):
+    message = f"Здравствуйте, {name}! Вы успешно подписались на нашу рассылку!"
+    send_mail(
+        'Подтверждение подписки',
+        message,
+        'partsauto312@gmail.com',
+        [email],
+        fail_silently=False,
+    )
