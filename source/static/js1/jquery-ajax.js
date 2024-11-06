d25b11c0f80acfd3363fb0d26b8a02cb912fc24f
Bekbol <19kani92@gmail.com>
2024-11-06 18:50:01 +0600

    увеличил время уведомления

--- source/static/js1/jquery-ajax.js
+++ source/static/js1/jquery-ajax.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
                 successMessage.fadeIn(400);
                 setTimeout(function () {
                     successMessage.fadeOut(400);
-                }, 2000);
+                }, 4000);
 
                 cartCount++;
                 goodsInCartCount.text(cartCount);
@@ -63,7 +63,7 @@ $(document).ready(function () {
                 successMessage.fadeIn(400);
                 setTimeout(function () {
                     successMessage.fadeOut(400);
-                }, 2000);
+                }, 4000);
 
                 cartCount -= data.quantity_deleted;
                 goodsInCartCount.text(cartCount);
@@ -120,7 +120,7 @@ $(document).ready(function () {
                 successMessage.fadeIn(400);
                 setTimeout(function () {
                     successMessage.fadeOut(400);
-                }, 7000);
+                }, 4000);
 
                 var goodsInCartCount = $("#goods-in-cart-count");
                 var cartCount = parseInt(goodsInCartCount.text() || 0);
@@ -142,7 +142,7 @@ $(document).ready(function () {
     if (notification.length > 0) {
         setTimeout(function () {
             notification.alert('close');
-        }, 1000);
+        }, 4000);
     }
 
     $('#modalButton').click(function () {
