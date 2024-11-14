51a0e7e99c9096ecdc670fbd566956e6f18bd079
Kairat <ischenbaew@gmai.com>
2024-11-14 22:03:59 +0100

    Поменял пароль доступа в почту

--- source/detail_shop/settings.py
+++ source/detail_shop/settings.py
@@ -187,4 +187,6 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "partsauto312@gmail.com"
-EMAIL_HOST_PASSWORD = "upfanzkikgofjghn"
+EMAIL_HOST_PASSWORD = "mmyh csmp tlrj szsa"
+
+
