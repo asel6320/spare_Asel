70ae85d4157d97d8472889f10aa9a802e8e9b068
Kairat <ischenbaew@gmai.com>
2024-11-14 22:02:26 +0100

    Добавил модель newsletter для массовой рассылки

--- source/newsletter/models.py
+++ source/newsletter/models.py
@@ -8,5 +8,14 @@ class Subscription(models.Model):
     subscription_date = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
 
+
     def __str__(self):
         return f"{self.name} <{self.email}>"
+
+class Newsletter(models.Model):
+    subject = models.CharField(max_length=255, verbose_name="Тема")
+    body = models.TextField(verbose_name="Сообщение")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return self.subject
