fe7550bc29bba7bc65efd9d9872cac086ee1e887
Kairat <ischenbaew@gmai.com>
2024-11-16 17:37:21 +0100

    База данных не поступали данные имени и имейл. Ошибка исправлена добавили формы новой поля

--- source/contacts/forms.py
+++ source/contacts/forms.py
@@ -5,16 +5,20 @@ from .models import ContactRequest
 class ContactRequestForm(forms.ModelForm):
     class Meta:
         model = ContactRequest
-        fields = ["last_name", "phone_number", "comments"]
+        fields = ["last_name", "phone_number", "comments","first_name","email"]
         widgets = {
+            "first_name": forms.TextInput(attrs={"placeholder": "Введите ваше имя"}),
             "last_name": forms.TextInput(attrs={"placeholder": "Введите вашу фамилию"}),
             "phone_number": forms.TextInput(
                 attrs={"placeholder": "Введите ваш номер телефона"}
             ),
+            "email": forms.EmailInput(attrs={"placeholder": "Введите ваш email"}),
             "comments": forms.TextInput(attrs={"placeholder": "Оставьте комментарии"}),
         }
         labels = {
+            "first_name":"Имя",
             "last_name": "Фамилия",
             "phone_number": "Номер телефона",
+            "email":"Email",
             "comments": "Комментарии",
         }
