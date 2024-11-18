8fee99777ffd87e062ba5b97af6c160c14be4e63
Bekbol <19kani92@gmail.com>
2024-11-18 19:54:22 +0600

    полностью закончил с детальным просмотром

--- source/webapp/forms/review_form.py
+++ source/webapp/forms/review_form.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.forms import widgets
 
 from webapp.models import Review
 
@@ -8,6 +7,14 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['text']
+        widgets = {
+            'text': forms.Textarea(attrs={
+                'rows': 4,
+                'placeholder': 'Введите ваш отзыв',
+                'class': 'form-control rounded-3',
+                'style': 'resize: none;',
+            }),
+        }
 
     def clean_text(self):
         text = self.cleaned_data.get('text')
