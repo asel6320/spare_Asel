74e3428914f948b59f2c3569ffcc0bd47c63f1fe
ABM1204 <bekabek13@bk.ru>
2024-11-20 15:44:25 +0600

    Изсенения для поставки

--- source/detail_shop/settings.py
+++ source/detail_shop/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['146.190.226.59']
 
 # Application definition
 
