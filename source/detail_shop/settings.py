62f6acd2bec51ea9f8c5b53c2316641ab9354c35
ABM1204 <bekabek13@bk.ru>
2024-11-27 16:55:34 +0600

    Настройка Докер + Nginx финал

--- source/detail_shop/settings.py
+++ source/detail_shop/settings.py
@@ -101,8 +101,8 @@ DATABASES = {
         'NAME': env.str("POSTGRES_DB"),
         'USER': env.str("POSTGRES_USER"),
         'PASSWORD': env.str("POSTGRES_PASSWORD"),
-        'HOST': env.str("HOST"),
-        #'HOST': "db",
+        #'HOST': env.str("HOST"),
+        'HOST': "db",
         'PORT': env.int("PORT"),
     }
 }
