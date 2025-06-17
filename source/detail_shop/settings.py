3cbb3bbcf9611552979d231cc2f366e3c923371e
Asel Amatova <asel6320@gmail.com>
2025-06-16 22:54:37 -0700

    downloaded whitenoise lib for deployment

--- source/detail_shop/settings.py
+++ source/detail_shop/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['146.190.226.59', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 # Application definition
 
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     "newsletter",
 ]
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
